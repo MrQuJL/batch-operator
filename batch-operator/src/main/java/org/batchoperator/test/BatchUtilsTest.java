70fe9807c0830e1f99701ca762a4e0b8bc3ef49b
MrQuJL <909301740@qq.com>
2018-04-12 17:22:11 +0800

    add comments for BatchUtilsTest

--- batch-operator/src/main/java/org/batchoperator/test/BatchUtilsTest.java
+++ batch-operator/src/main/java/org/batchoperator/test/BatchUtilsTest.java
@@ -6,8 +6,18 @@ import java.util.Map;
 import org.batchoperator.util.BatchUtils;
 import org.junit.Test;
 
+/**
+ * 类描述：对BatchUtils工具类进行测试
+ * 类名称：org.batchoperator.test.BatchUtilsTest
+ * @author 曲健磊
+ * 2018年4月12日.下午5:20:15
+ * @version V1.0
+ */
 public class BatchUtilsTest {
 
+	/**
+	 * 测试生成单条插入语句
+	 */
 	@Test
 	public void testGenerateSqlOfInsert() {
 		Map<String, String> columnMap = new HashMap<String, String>();
@@ -21,6 +31,9 @@ public class BatchUtilsTest {
 		// INSERT INTO pms_user(user_id) VALUES(1)
 	}
 	
+	/**
+	 * 测试生成多条插入语句
+	 */
 	@Test
 	public void testGenerateSqlOfBatchInsert() {
 		Map<String, String> columnMap = new HashMap<String, String>();
@@ -36,6 +49,7 @@ public class BatchUtilsTest {
 //		INSERT INTO pms_user(user_id ,user_name ,user_age) VALUES(1 ,1 ,1);
 //		INSERT INTO pms_user(user_id ,user_name ,user_age) VALUES(1 ,1 ,1);
 //		INSERT INTO pms_user(user_id ,user_name ,user_age) VALUES(1 ,1 ,1);
+		
 	}
 
 }
